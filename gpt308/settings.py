0712697edfc804e2bb6accbf8c7132f72a1fb6b2
Sayed Sadat <sayedsadat98@gmail.com>
2024-02-29 12:41:28 -0500

    Configure STATIC_ROOT

--- gpt308/settings.py
+++ gpt308/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,6 +136,7 @@ EMAIL_HOST_PASSWORD = "fwdw habd bdxl kxlf"
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
